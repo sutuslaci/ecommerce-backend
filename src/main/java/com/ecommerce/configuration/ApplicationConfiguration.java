8317c840c3f4f8ce2c79a9e833698dac73a38784
Sutus László <h051618@stud.u-szeged.hu>
2024-04-29 14:42:17 +0200

    Add CORS origin.

--- src/main/java/com/ecommerce/configuration/ApplicationConfiguration.java
+++ src/main/java/com/ecommerce/configuration/ApplicationConfiguration.java
@@ -25,7 +25,10 @@ public class ApplicationConfiguration {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+                registry.addMapping("/**").allowedOrigins(
+                        "http://localhost:4200",
+                        "https://jolly-grass-0248a7b03.5.azurestaticapps.net"
+                );
             }
         };
     }
